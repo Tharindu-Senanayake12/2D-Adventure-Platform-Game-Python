ddd06088a817c7b087b0e14681570810059d2355
Tharindu Senanayake <tharindusenanayake134@gmail.com>
2025-11-02 12:50:59 +0530

    Initial commit - jungle adventure game

---
+++ level.py
@@ -0,0 +1,19 @@
+# level.py
+
+TILE_SIZE = 64
+
+# Level map:
+# P = Player, E = Enemy, C = Coin, # = Tile, T = Tree
+level_map = [
+    "                                    C                                            ",
+    "                                    C                                             ",
+    "          E CR                      C R                               C                                    ",
+    "         #######                    ######     C                    C   CT                                 ",
+    "    T C C                                     R T            C        ######                                ",
+    "   ###   C          CT                       ######               R   IIIIII                                ",
+    "          C       #####      C             E IIIIII      ##   ######  IIIIII                                ",
+    "  P     ####                           ##############                 IIIIII###       C     C C C                    ",
+    "                   E     T          #  IIIIIIIIIIIIII   E             IIIIIIIII   E      T        E   R          ",
+    "############################   ############################           ######################   #########################     "
+] 
+ 
